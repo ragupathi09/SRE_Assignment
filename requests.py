55e2517a6ff0185ce2c463ed46ed1c9d8428dce1
Mahathi <97830392+ragupathi09@users.noreply.github.com>
2025-02-04 00:24:37 +0530

    Create requests.py

---
+++ requests.py
@@ -0,0 +1,31 @@
+
+import requests
+
+ALLOWED_VALUES = ["Data", "Processing", "Web"]
+
+def get_allowed_service_values():
+
+# tags value am not set here if tags are match then get resposponce will work
+      if (tags_value in ALLOWED_VALUS):
+      
+           url = "http://<your-private-endpoint>/allowed-services"
+          
+          try:
+              response = requests.get(url)
+              response.raise_for_status()  
+              data = response.json()
+              allowed_services = data.get("ALLOWED_VALUES", [])
+              return allowed_services
+          except requests.RequestException as e:
+              print(f"Error during GET request: {e}")
+              return None
+
+
+allowed_values = get_allowed_service_values()
+    if allowed_values:
+        print("Allowed Service Tag Values:", allowed_values)
+    else:
+        print("Failed to retrieve allowed service values.")
+
+
+
